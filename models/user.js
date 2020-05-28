df1a7e776198918a53175004a85e2e03ea8fde44
Brandon Davis <davis.brandon821@gmail.com>
2020-05-27 21:35:34 -0400

    user model

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const user = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 5 },
+  })
+);
+
+module.exports = user;
